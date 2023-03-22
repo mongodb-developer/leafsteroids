8bf0e7ca7b8d9021e715832850604380ff766db0
Dominic Frei <business.dominic.frei@gmail.com>
2023-03-22 08:00:12 -0700

    Adjust email

--- blazor_registration/gdc_2023_demo_registration/Data/Team.cs
+++ blazor_registration/gdc_2023_demo_registration/Data/Team.cs
@@ -1,9 +1,11 @@
 using MongoDB.Bson;
 
+namespace gdc_2023_demo_registration.Data;
+
 public class Team
 {
     public ObjectId Id { get; set; }
-    public string Nickname { get; set; }
-    public string TeamName { get; set; }
-    public string Slogan { get; set; }
+    public string? Nickname { get; set; }
+    public string? TeamName { get; set; }
+    public string? Email { get; set; }
 }
