6baf27491b2e53ceccf242cf72acce88c3b2c8a3
Dominic Frei <business.dominic.frei@gmail.com>
2023-05-11 15:42:50 +0100

    Schema adjustments.

--- unity_game/Assets/Scripts/00_Shared/RegisteredPlayer.cs
+++ unity_game/Assets/Scripts/00_Shared/RegisteredPlayer.cs
@@ -1,12 +1,14 @@
 using JetBrains.Annotations;
+using Newtonsoft.Json;
 
 namespace _00_Shared
 {
     public class RegisteredPlayer
     {
         [CanBeNull] public string Email;
+        [JsonProperty("_id")]
         public string Id;
-        [CanBeNull] public string location;
+        // [CanBeNull] public string location;
         public string Nickname;
         [CanBeNull] public string TeamName;
     }
--- unity_game/Assets/Scripts/3_Main/PlayerHitsEnemy.cs
+++ unity_game/Assets/Scripts/3_Main/PlayerHitsEnemy.cs
@@ -6,11 +6,17 @@ namespace _3_Main
     {
         [SerializeField] private GameManager gameManager;
 
+        private bool playerHitEnemy;
+
         private void OnCollisionEnter(Collision collision)
         {
             if (collision == null) return;
             if (collision.gameObject == null) return;
             if (collision.gameObject.GetComponentInParent<Enemy>() == null) return;
+            
+            if (playerHitEnemy) return;
+            playerHitEnemy = true;
+            
             SessionStatistics.Instance!.Score = 0;
             gameManager!.GameOver();
         }
