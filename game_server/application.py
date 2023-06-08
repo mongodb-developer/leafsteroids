a2d27e617d0202b10b8d0a107a82c265d2912acb
Dominic Frei <business.dominic.frei@gmail.com>
2023-06-08 18:21:24 +0100

    Update logging.

--- game_server/application.py
+++ game_server/application.py
@@ -9,10 +9,8 @@ from flask import Flask, jsonify, request
 
 from constants import ENDPOINT_GET_EVENTS, ENDPOINT_GET_CONFIG, ENDPOINT_GET_PLAYERS, ENDPOINT_POST_SNAPSHOT
 
+logging.basicConfig(filename='record.log', level=logging.DEBUG)
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-logging.basicConfig(level=logging.DEBUG)
-app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/', methods=['GET'])
@@ -52,8 +50,11 @@ def send_request(route: str, endpoint: str, json_object: Optional[Any] = None):
             result = requests.post(endpoint, headers=headers, json=json_object)
             app.logger.debug(f"POST request to {endpoint} resulted in: {result.status_code}")
             if result.status_code == 200:
+                app.logger.info(f"Returning: {json_object=}")
                 return result.json()
+
             else:
+                app.logger.info(f"Returning status code {result.status_code}")
                 return result.status_code
 
         else:
@@ -61,8 +62,11 @@ def send_request(route: str, endpoint: str, json_object: Optional[Any] = None):
             json_string = result.content.decode()
             json_object = json.loads(json_string)
             if not result or not result.content:
+                app.logger.info(f"Returning status code 404")
                 return jsonify({'error': f'No {route} found.'}), 404
+
             else:
+                app.logger.info(f"Returning {json_object=}")
                 return jsonify(json_object)
 
     except Exception as exception:
