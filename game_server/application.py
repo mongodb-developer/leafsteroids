2ed3ffe99bf09a58d7a78270834cc3d1a336d99c
Dominic Frei <business.dominic.frei@gmail.com>
2023-06-28 13:54:57 +0100

    Update game server.

--- game_server/application.py
+++ game_server/application.py
@@ -1,13 +1,15 @@
 import json
 import logging
 import os
-import sys
 from typing import Optional, Any
 
 import requests
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
 from constants import ENDPOINT_GET_EVENTS, ENDPOINT_GET_CONFIG, ENDPOINT_GET_PLAYERS, ENDPOINT_POST_SNAPSHOT
+
+load_dotenv()
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG)
 app = Flask(__name__)
@@ -15,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_root():
-    return "I'm alive!"
+    return "I'm alive!\n"
 
 
 @app.route('/events', methods=['GET'])
@@ -40,36 +42,37 @@ def post_recording():
 
 def send_request(route: str, endpoint: str, json_object: Optional[Any] = None):
     try:
-        app.logger.debug(f"send_request called:")
-        app.logger.debug(f"{route}")
-        app.logger.debug(f"{endpoint}")
-        app.logger.debug(f"{json_object}")
+        app.logger.debug(f"send_request called:\n"
+                         f"{route=}\n"
+                         f"{endpoint=}\n"
+                         f"{json_object=}")
+
         headers = {"apiKey": os.environ["API_KEY"]}
 
-        if json_object is not None:
+        if json_object is None:
+            result = requests.get(endpoint, headers=headers)
+            app.logger.debug(f"GET request to {endpoint} resulted in: {result.status_code}")
+            json_string = result.content.decode()
+            json_object = json.loads(json_string)
+            if not result or not result.content:
+                app.logger.info(f"Returning status code 404")
+                return jsonify({'error': f'No {route=} found (internal status code: {result.status_code}).'}), 404
+            else:
+                app.logger.info(f"Returning {json_object=}")
+                return jsonify(json_object)
+
+        else:
             result = requests.post(endpoint, headers=headers, json=json_object)
             app.logger.debug(f"POST request to {endpoint} resulted in: {result.status_code}")
             if result.status_code == 200:
                 app.logger.info(f"Returning: {json_object=}")
                 return result.json()
-
             else:
                 app.logger.info(f"Returning status code {result.status_code}")
                 return result.status_code
 
-        else:
-            result = requests.get(endpoint, headers=headers)
-            json_string = result.content.decode()
-            json_object = json.loads(json_string)
-            if not result or not result.content:
-                app.logger.info(f"Returning status code 404")
-                return jsonify({'error': f'No {route} found.'}), 404
-
-            else:
-                app.logger.info(f"Returning {json_object=}")
-                return jsonify(json_object)
-
     except Exception as exception:
+        app.logger.error(f"{exception=}")
         return jsonify({'error': f'Internal Server Error:\n{exception=}'}), 500
 
 
