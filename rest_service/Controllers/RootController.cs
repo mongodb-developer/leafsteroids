d57e114c0dbf2a4497e823cef86723f68d5f14de
Dominic Frei <business.dominic.frei@gmail.com>
2023-08-03 02:31:21 +0100

    I'm alive!

---
+++ rest_service/Controllers/RootController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestService.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class RootsController : BaseController
+    {
+        public RootsController(ILogger<RootsController> logger) : base(logger)
+        {
+        }
+
+        [HttpGet(Name = "GetRoot")]
+        public string GetRoot()
+        {
+            Logger.LogDebug($"Route {nameof(GetRoot)} called.");
+
+            return "I'm alive!";
+        }
+    }
+}
