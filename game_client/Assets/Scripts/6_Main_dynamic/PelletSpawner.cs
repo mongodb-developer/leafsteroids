5a57ecf07e46859be9a977613f24bf8b466d7869
Dominic Frei <business.dominic.frei@gmail.com>
2023-08-18 15:22:47 +0100

    Spawn initial pallet.

--- game_client/Assets/Scripts/6_Main_dynamic/PelletSpawner.cs
+++ game_client/Assets/Scripts/6_Main_dynamic/PelletSpawner.cs
@@ -4,12 +4,14 @@ namespace _6_Main_dynamic
 {
     public class PelletSpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject PalletLargeContainerPrefab;
+        [SerializeField] private GameObject palletParent;
+        [SerializeField] private GameObject palletLargeContainerPrefab;
 
 
         private void Start()
         {
-            Instantiate(PalletLargeContainerPrefab);
+            Instantiate(palletLargeContainerPrefab, new Vector3(3f, 0.05f, 3f), Quaternion.identity,
+                palletParent.gameObject.transform);
         }
     }
 }
