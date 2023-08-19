b6bde6f2b781d11af4733ea248bf4a5649fd727a
Sig Narváez <sigfrido.narvaez@10gen.com>
2023-08-18 22:42:49 -0700

    players_unique patch
    _id now stores the Nickname, and is a unique shard key.
    Added index for covered queries too

--- rest_service/Entities/PlayerUnique.cs
+++ rest_service/Entities/PlayerUnique.cs
@@ -8,38 +8,8 @@ namespace RestService.Entities
     public class PlayerUnique
     {
         [BsonId]
-        [JsonProperty("_id")]
-        public PlayerUniqueId Id { get; set; }
-
-        [JsonProperty("nickname")]
-        [BsonIgnore]
-        public string? Name
-        {
-            get { return Id.Name; }
-            set { Id.Name = value; }
-        }
-
-        [JsonProperty("location")]
-        [BsonIgnore]
-        public string? Location {
-            get { return Id.Location; }
-            set { Id.Location = value; }
-        }
-
-        public PlayerUnique() { Id = new PlayerUniqueId(); }
-
-        public PlayerUnique(string nickname, string location) : this()
-        {
-            Name = nickname;
-            Location = location;
-        }
-    }
-
-    public class PlayerUniqueId
-    {
-        [BsonElement("Nickname")]
-        [JsonProperty("name")]
-        public string? Name { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
 
         [JsonProperty("location")]
         [BsonElement("location")]
