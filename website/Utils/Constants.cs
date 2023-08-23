9d480087a06dd6fd3afea90c408f8b45c97b228f
Sig Narváez <sigfrido.narvaez@10gen.com>
2023-08-23 16:47:23 -0700

    Data Tiering for STAGE

--- website/Utils/Constants.cs
+++ website/Utils/Constants.cs
@@ -9,14 +9,24 @@ public static class Constants
         "https://charts.mongodb.com/charts-global_shared-jebkk/embed/dashboards";
 
     // STAGING
-    //public const string AtlasChartIdEvent = "64c95022-391d-4240-8d73-b033c8e34195"; 
-    //public const string AtlasChartIdPlayer = "64c95045-413c-4e46-8c40-f033169d4011"; 
-    //public const string AtlasChartIdHome = "64c95059-aef7-4a35-86e8-ea27edd5b50b";
+    /*
+    public const string AtlasChartIdEvent = "64c95022-391d-4240-8d73-b033c8e34195"; 
+    public const string AtlasChartIdPlayer = "64c95045-413c-4e46-8c40-f033169d4011"; 
+    public const string AtlasChartIdHome = "64c95059-aef7-4a35-86e8-ea27edd5b50b";
+    */
+
+    // STAGING WITH DATA TIERING
+    /*
+    public const string AtlasChartIdEvent = "64e690fe-8a52-40f6-8961-487724e5d868";
+    public const string AtlasChartIdPlayer = "64e69107-a6a2-4aae-828b-caf12cc17fc6";
+    public const string AtlasChartIdHome = "64e691bd-8a52-47ef-827a-487724e690aa";
+    */
 
     // PRODUCTION
     public const string AtlasChartIdEvent = "6453fb66-c2fc-4212-8dbd-4ab8365f1ac0";
     public const string AtlasChartIdPlayer = "64540ea3-f8b8-4211-8c3b-cff2506301a4";
     public const string AtlasChartIdHome = "645abbc7-2258-4908-8eb4-94bbe0c6d324";
+
 
     public const string RestServiceEndpointEvents = "events";
     public const string RestServiceEndpointPlayers = "players";
