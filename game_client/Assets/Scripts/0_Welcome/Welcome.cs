1898fadc1a16f8a033b72a642855ca2ee6538568
Carlos Castro <carlos.castro@mongodb.com>
2023-11-22 20:56:09 +0000

    add qr code to play screen;

--- game_client/Assets/Scripts/0_Welcome/Welcome.cs
+++ game_client/Assets/Scripts/0_Welcome/Welcome.cs
@@ -1,5 +1,6 @@
 using System;
 using _1_Loading;
+using _00_Shared;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,28 +15,6 @@ namespace _0_Welcome
         [SerializeField] private TMP_Text welcomeTextField;
         [SerializeField] private Image image;
 
-        public Sprite LoadNewSprite(string filePath, float pixelsPerUnit = 100.0f, SpriteMeshType spriteType = SpriteMeshType.Tight)
-        {
-            // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
-            Texture2D spriteTexture = LoadTexture(filePath);
-            Sprite newSprite = Sprite.Create(spriteTexture, new Rect(0, 0, spriteTexture.width, spriteTexture.height), new Vector2(0, 0), pixelsPerUnit, 0, spriteType);
-            return newSprite;
-        }
-        public Texture2D LoadTexture(string filePath)
-        {
-            // Load a PNG or JPG file from disk to a Texture2D
-            // Returns null if load fails
-            Texture2D tex2D;
-            byte[] fileData;
-            if (File.Exists(filePath))
-            {
-                fileData = File.ReadAllBytes(filePath);
-                tex2D = new Texture2D(2, 2);           // Create new "empty" texture
-                if (tex2D.LoadImage(fileData))           // Load the imagedata into the texture (size is set automatically)
-                    return tex2D;                 // If data = readable -> return texture
-            }
-            return null;                     // Return null if load failed
-        }
         IEnumerator GetEventQR(string uri)
         {
             using (UnityWebRequest uwr = UnityWebRequest.Get(uri))
@@ -54,7 +33,7 @@ namespace _0_Welcome
                     File.WriteAllBytes(path, uwr.downloadHandler.data);
                     Debug.Log("File saved at: " + path);
 
-                    Sprite sprite = LoadNewSprite(path);
+                    Sprite sprite = LoadSprite.LoadNewSprite(path);
                     image!.sprite = sprite;
 
                     welcomeTextField!.text = $"Welcome to\n{GameConfigLoader.Instance!.GameConfig!.Event!.Name}";
