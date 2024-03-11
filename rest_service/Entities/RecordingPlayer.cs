b679494bc8ab5ea92175fa8a54abacb279342582
Sig Narváez <sigfrido.narvaez@10gen.com>
2024-03-11 10:39:12 -0700

    Serialization fixes

--- rest_service/Entities/RecordingPlayer.cs
+++ rest_service/Entities/RecordingPlayer.cs
@@ -1,22 +1,29 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Serializers;
 
 namespace RestService.Entities;
 
+[BsonIgnoreExtraElements]
 public class RecordingPlayer : Player
 {
     // This class is empty intentionally.
     // Its purpose is to override the serialization to store a subset of the parent class
     // Thus implementing the Extended Reference Schema Design Pattern:
-    // https://www.mongodb.com/blog/post/building-with-patterns-the-extended-reference-pattern 
+    // https://www.mongodb.com/blog/post/building-with-patterns-the-extended-reference-pattern
 }
 
-public class PlayerForRecordingsSerializer : SerializerBase<RecordingPlayer>
+public class RecordingPlayerSerializer : SerializerBase<RecordingPlayer>, IBsonDocumentSerializer
 {
     public override RecordingPlayer Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
-        return BsonSerializer.Deserialize<RecordingPlayer>(context.Reader);
+        var deserialized = BsonDocumentSerializer.Instance.Deserialize(context);
+        return new RecordingPlayer()
+        {
+            Name = deserialized.GetValue("Nickname").ToString(),
+            Location = deserialized.GetValue("location").ToString()
+        };
     }
 
     public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, RecordingPlayer value)
@@ -28,5 +35,30 @@ public class PlayerForRecordingsSerializer : SerializerBase<RecordingPlayer>
             };
         
         BsonDocumentSerializer.Instance.Serialize(context, document);
+    }
+
+    public bool TryGetMemberSerializationInfo(string memberName, out BsonSerializationInfo? serializationInfo)
+    {
+        switch (memberName)
+        {
+            case "Id":
+                serializationInfo = new BsonSerializationInfo("_id", new ObjectIdSerializer(), typeof(ObjectId));
+                return true;
+            case "Name":
+                serializationInfo = new BsonSerializationInfo("Nickname", new StringSerializer(), typeof(string));
+                return true;
+            case "Team":
+                serializationInfo = new BsonSerializationInfo("TeamName", new StringSerializer(), typeof(string));
+                return true;
+            case "Email":
+                serializationInfo = new BsonSerializationInfo("Email", new StringSerializer(), typeof(string));
+                return true;
+            case "Location":
+                serializationInfo = new BsonSerializationInfo("location", new StringSerializer(), typeof(string));
+                return true;
+            default:
+                serializationInfo = null;
+                return false;
+        }
     }
 }
